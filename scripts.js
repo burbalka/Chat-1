25a6e25f317aa9ed3de445675b18e0c5ca22f1a3
Angelina-Panteley <lina.angel.95@mail.ru>
2015-03-09 20:50:00 +0300

    Новые кнопки удаления-редактирования

--- scripts.js
+++ scripts.js
@@ -11,28 +11,24 @@ function changeLogin()
 	  document.getElementById("loginChanged").innerHTML=text2;
 	  }
  }
- function changeLogin()
-   {
-      var text, text2;
-	  text=document.getElementById("login").value;
-	  if(text!=""){
-	  text2="Welcome to SweetChat, "+text+"!!";
-	  
-	  document.getElementById("loginChanged").innerHTML=text2;
-	  }
-   }
+
   function addRow() {
           
-    var myName = document.getElementById("mail");
-    var table = document.getElementById("myTableData");
+//   var myMail = document.getElementById("mail");
+ //   var table = document.getElementById("myTableData");
  
-    var rowCount = table.rows.length;
-    var row = table.insertRow(rowCount);
- 
-    row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
-    row.insertCell(1).innerHTML= myName.value;
+ //   var rowCount = table.rows.length;
+ //   var row = table.insertRow(rowCount);
     
- 
+ //   row.insertCell(0).innerHTML= '<p align="right"><input class="button5" type="submit" value ="" onClick="Javacsript:deleteRow(this)" title="Удалить сообщение" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI) center center no-repeat;"></input><input class="button5" type="submit" title="Редактировать выделенное сообщение" value="" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A) center center no-repeat;"></input></p>';
+	
+ //   row.insertCell(1).innerHTML= myMail.value;
+    
+ var myMail = document.getElementById("mail");
+ var tableContent="<tr>";
+ tableContent+="<td width=60%>"+'<p align="right"><input class="button5" type="submit" value ="" onClick="Javacsript:deleteRow(this)" title="Удалить сообщение" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI) center center no-repeat;"></input><input class="button5" type="submit" title="Редактировать выделенное сообщение" value="" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A) center center no-repeat;"></input></p>'+"</td>";
+ tableContent+="<td width=40%>"+myMail.value+"</td>";
+ myTableData.innerHTML+=tableContent;
 }
  
 function deleteRow(obj) {
