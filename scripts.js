2c36f5608499a297c5331cef75b7d6a6e2fccb02
Angelina-Panteley <lina.angel.95@mail.ru>
2015-03-09 23:15:09 +0300

    Edit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+var index=-1;
 function run(){
 
 }
@@ -6,34 +7,55 @@ function changeLogin()
       var text, text2;
 	  text=document.getElementById("login").value;
 	  if(text!=""){
-	  text2="Welcome to SweetChat, "+text+"!";
-	  
-	  document.getElementById("loginChanged").innerHTML=text2;
+	      text2="Welcome to SweetChat, "+text+"!";
+	      document.getElementById("loginChanged").innerHTML=text2;
 	  }
  }
 
   function addRow() {
           
-//   var myMail = document.getElementById("mail");
- //   var table = document.getElementById("myTableData");
- 
- //   var rowCount = table.rows.length;
- //   var row = table.insertRow(rowCount);
+   var myName = document.getElementById("mail");
+   var table = document.getElementById("myTableData");
+ if(index<0)
+    {      
+   	 var rowCount = table.rows.length;
+   	 var row = table.insertRow(rowCount);
+	 row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+  	 row.insertCell(1).innerHTML= '<input type="button" value = "Edit" onClick="Javacsript:editRow(this)">';
+ 	 row.insertCell(2).innerHTML = myName.value;  
+}
+    if(index>-1)
+    {
+	table.deleteRow(index);   
+	var row = table.insertRow(index);
+	row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+	row.insertCell(1).innerHTML= '<input type="button" value = "Edit" onClick="Javacsript:editRow(this)">';  
+	row.insertCell(2).innerHTML = myName.value;
+index=-1;
+    }
+ //  var rowCount = table.rows.length;
+ //  var row = table.insertRow(rowCount);
     
- //   row.insertCell(0).innerHTML= '<p align="right"><input class="button5" type="submit" value ="" onClick="Javacsript:deleteRow(this)" title="Удалить сообщение" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI) center center no-repeat;"></input><input class="button5" type="submit" title="Редактировать выделенное сообщение" value="" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A) center center no-repeat;"></input></p>';
-	
- //   row.insertCell(1).innerHTML= myMail.value;
+ // row.insertCell(0).innerHTML= '<input class="button5" type="submit" value ="" onClick="Javacsript:deleteRow(this)" title="Удалить сообщение" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI) center center no-repeat;"></input><input class="button5" type="submit" title="Редактировать выделенное сообщение" value="" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A) center center no-repeat;"></input>';
+
+	//row.insertCell(0).innerHTML= '<input type="button" value = "Delete" onClick="Javacsript:deleteRow(this)">';
+//	if(document.getElementById('login').value=="")
+// {
+//	 document.getElementById('login').value="User";
+// }
+ //   row.insertCell(1).innerHTML= "<font>"+document.getElementById('login').value+":"+"</font>";
+// row.insertCell(2).innerHTML= myName.value;
    
- var myMail = document.getElementById("mail");
+ //var myMail = document.getElementById("mail");
  
- var tableContent="<tr>";
- tableContent+="<td width=60% valign='bottom'>"+'<p align="right"><input class="button5" type="submit" value ="" onClick="Javacsript:deleteRow(this)" title="Удалить сообщение" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI) center center no-repeat;"></input><input class="button5" type="submit" title="Редактировать выделенное сообщение" value="" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A) center center no-repeat;"></input></p>'+"</td>";
- if(document.getElementById('login').value=="")
- {
-	 document.getElementById('login').value="User";
- }
- tableContent+="<td width=40% >"+"<font color='black' align='right' size=4px style='vertical-align:'top';'>"+"<b>"+document.getElementById('login').value+":"+"</b>"+"</font>"+"<br>"+myMail.value+"</td>";
- myTableData.innerHTML+=tableContent;
+ //var tableContent="<tr>";
+ //tableContent+="<td width=60% valign='bottom'>"+'<p align="right"><input class="button5" type="submit" value ="" onClick="Javacsript:deleteRow(this)" title="Удалить сообщение" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/eraser.png?itok=ohy0hMWI) center center no-repeat;"></input><input class="button5" type="submit" title="Редактировать выделенное сообщение" value="" style="background: url(http://www.defaulticon.com/sites/default/files/styles/icon-front-page-32x32-preview/public/field/image/edit.png?itok=nb2eY85A) center center no-repeat;"></input></p>'+"</td>";
+ //if(document.getElementById('login').value=="")
+ //{
+//	 document.getElementById('login').value="User";
+ //}
+// tableContent+="<td width=40% >"+"<font color='black' align='right' size=4px style='vertical-align:'top';'>"+"<b>"+document.getElementById('login').value+":"+"</b>"+"</font>"+"<br>"+myMail.value+"</td>";
+// myTableData.innerHTML+=tableContent;
  document.getElementById('mail').value='';
 }
  
@@ -44,9 +66,13 @@ function deleteRow(obj) {
     table.deleteRow(index);
     
 }
- 
+ function editRow(obj) {
+    index = obj.parentNode.parentNode.rowIndex;
+    var table = document.getElementById("myTableData");
+    document.getElementById("mail").value=table.rows[index].cells[2].innerHTML;
+}
 function load() {
-    
+    //changeLogin();
     console.log("Page load finished");
- 
+    
 }
